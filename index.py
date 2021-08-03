4230e649d4faec0e68ff9482026fe47e8dcf2204
Simon Willison <swillison@gmail.com>
2021-08-03 11:09:09 -0700

    Added /delete-all-cookies feature

--- index.py
+++ index.py
@@ -35,6 +35,10 @@ async def homepage(request):
       <input type="submit" value="Set cookie with no SameSite at all">
       </p>
     </form>
+    <form action="/delete-all-cookies" method="POST">
+      <input type="submit" value="Delete ALL cookies">
+      </p>
+    </form>
     <p>Once you have set some cookies visit <a href="https://simonw.github.io/samesite-lax-demo/">https://simonw.github.io/samesite-lax-demo/</a> to try navigating back to this page.</p>
     <p>More information in <a href="https://github.com/simonw/samesite-lax-demo/blob/main/README.md">this README</a>.</p>
     <p>Cookies as an SVG image:</p>
@@ -48,7 +52,9 @@ async def set_cookie(request, samesite="lax", secure=False):
     form_vars = await request.form()
     demo = form_vars.get("demo") or ""
     response = RedirectResponse("/", status_code=302)
-    response.set_cookie("{}-demo".format(samesite or "missing"), demo, samesite=samesite, secure=secure)
+    response.set_cookie(
+        "{}-demo".format(samesite or "missing"), demo, samesite=samesite, secure=secure
+    )
     return response
 
 
@@ -64,20 +70,26 @@ async def set_missing(request):
     return await set_cookie(request, samesite=None)
 
 
+async def delete_all_cookies(request):
+    response = RedirectResponse("/", status_code=302)
+    for cookie in request.cookies:
+        response.delete_cookie(cookie)
+    return response
+
+
 async def cookies_svg(request):
     lines = []
     for i, (name, value) in enumerate(request.cookies.items()):
         lines.append(
             '<text x="10" y="{y}" style="font: 4px sans-serif;">{name}={value}</text>'.format(
-                y = 10 + (i * 6),
-                name=html.escape(name),
-                value=html.escape(value)
+                y=10 + (i * 6), name=html.escape(name), value=html.escape(value)
             )
         )
     return Response(
         '<svg viewBox="0 0 400 80" xmlns="http://www.w3.org/2000/svg">{}</svg>'.format(
-            '\n'.join(lines)
-        ),media_type="image/svg+xml;charset=utf-8",
+            "\n".join(lines)
+        ),
+        media_type="image/svg+xml;charset=utf-8",
     )
 
 
@@ -88,6 +100,7 @@ app = Starlette(
         Route("/set-none", set_none, methods=["POST"]),
         Route("/set-strict", set_strict, methods=["POST"]),
         Route("/set-missing", set_missing, methods=["POST"]),
+        Route("/delete-all-cookies", delete_all_cookies, methods=["POST"]),
         Route("/cookies.svg", cookies_svg, methods=["GET"]),
     ]
 )
